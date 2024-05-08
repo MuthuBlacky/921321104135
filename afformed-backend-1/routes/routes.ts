7ac678808f7b861cea9b50afb2ab648bb908cc1d
MuthuBlacky <muthupandid21cs@psnacet.edu.in>
2024-05-08 17:09:44 +0530

    static to dynamic fetching completed datas are from server

--- afformed-backend-1/routes/routes.ts
+++ afformed-backend-1/routes/routes.ts
@@ -1,20 +1,21 @@
 import express from "express";
 require('dotenv').config();
 export const router = express.Router();
-router.route('/:categoryname/products').get((req,res,next)=>{
-    
 
-})
-
-router.route('/:categoryname/:n/products').get((req,res,next) => {
+router.route('/:companyname/:categoryname/:n/products').get((req,res,next) => {
     try{
+      console.log(typeof req.params.n)
+      const startRange = parseInt(req.body.startRange);
+      const endRange = parseInt(req.body.endRange);
     const numberOfProduct =  parseInt(req.params.n)
+    console.log(numberOfProduct)
         if(numberOfProduct > 10){
             res.status(400)
             throw new Error("Number of product is more then 10 use page limit")
         }
         const token = process.env.TOKEN
-        fetch(`http://20.244.56.144/test/companies/:companyname/categories/${req.params.categoryname}/products?top=${numberOfProduct}&minPrice=p&maxPrice=q`,{
+        console.log(token)
+        fetch(`http://20.244.56.144/test/companies/${req.params.companyname}/categories/${req.params.categoryname}/products?top=${numberOfProduct}&minPrice=${startRange}&maxPrice=${endRange}`,{
             method : "GET",
             headers: {
                 'Authorization': `Bearer ${token}`
@@ -31,176 +32,26 @@ router.route('/:categoryname/:n/products').get((req,res,next) => {
     }
 })
 
-router.route('/:categoryname/:n/:page/products').get((req,res,next)=>{
-  const page = parseInt(req.params.page);
-  const numberOfPages = parseInt(req.params.n);
-  const data = [
-    {
-      productName : "Laptop 1",
-      price : 2236,
-      rating:4.7,
-      discount : 63,
-      availability :"yes"
-    },
-    {
-      productName : "Laptop 13",
-      price :1424,
-      rating:4.5,
-      discount : 14,
-      availability :"out-of-stock"
-    },
-    {
-      productName : "Laptop 3",
-      price : 9013,
-      rating:4.7,
-      discount : 63,
-      availability :"yes"
-    },
-    {
-        productName : "Laptop 1",
-        price : 2236,
-        rating:4.7,
-        discount : 63,
-        availability :"yes"
-      },
-      {
-        productName : "Laptop 13",
-        price :1424,
-        rating:4.5,
-        discount : 14,
-        availability :"out-of-stock"
-      },
-      {
-        productName : "Laptop 3",
-        price : 9013,
-        rating:4.7,
-        discount : 63,
-        availability :"yes"
-      },
-      {
-        productName : "Laptop 1",
-        price : 2236,
-        rating:4.7,
-        discount : 63,
-        availability :"yes"
-      },
-      {
-        productName : "Laptop 13",
-        price :1424,
-        rating:4.5,
-        discount : 14,
-        availability :"out-of-stock"
-      },
-      {
-        productName : "Laptop 3",
-        price : 9013,
-        rating:4.7,
-        discount : 63,
-        availability :"yes"
-      },
-      {
-        productName : "Laptop 1",
-        price : 2236,
-        rating:4.7,
-        discount : 63,
-        availability :"yes"
-      },
-      {
-        productName : "Laptop 13",
-        price :1424,
-        rating:4.5,
-        discount : 14,
-        availability :"out-of-stock"
-      },
-      {
-        productName : "Laptop 3",
-        price : 9013,
-        rating:4.7,
-        discount : 63,
-        availability :"yes"
-      },
-      {
-        productName : "Laptop 1",
-        price : 2236,
-        rating:4.7,
-        discount : 63,
-        availability :"yes"
-      },
-      {
-        productName : "Laptop 13",
-        price :1424,
-        rating:4.5,
-        discount : 14,
-        availability :"out-of-stock"
-      },
-      {
-        productName : "Laptop 3",
-        price : 9013,
-        rating:4.7,
-        discount : 63,
-        availability :"yes"
-      },
-      {
-        productName : "Laptop 1",
-        price : 2236,
-        rating:4.7,
-        discount : 63,
-        availability :"yes"
-      },
-      {
-        productName : "Laptop 13",
-        price :1424,
-        rating:4.5,
-        discount : 14,
-        availability :"out-of-stock"
-      },
-      {
-        productName : "Laptop 3",
-        price : 9013,
-        rating:4.7,
-        discount : 63,
-        availability :"yes"
-      },
-      {
-        productName : "Laptop 1",
-        price : 2236,
-        rating:4.7,
-        discount : 63,
-        availability :"yes"
-      },
-      {
-        productName : "Laptop 13",
-        price :1424,
-        rating:4.5,
-        discount : 14,
-        availability :"out-of-stock"
-      },
-      {
-        productName : "Laptop 3",
-        price : 9013,
-        rating:4.7,
-        discount : 63,
-        availability :"yes"
-      }
-    ]
-    console.log(JSON.stringify(data))
+router.route('/:companyname/:categoryname/:n/:page/products').get((req,res,next)=>{
     const token = process.env.TOKEN
     const numberOfProduct =  parseInt(req.params.n)
-    fetch(`http://20.244.56.144/test/companies/:companyname/categories/${req.params.categoryname}/products?top=${numberOfProduct}&minPrice=p&maxPrice=q`,{
+    const startRange = parseInt(req.body.startRange);
+    const endRange = parseInt(req.body.endRange);
+    fetch(`http://20.244.56.144/test/companies/${req.params.companyname}/categories/${req.params.categoryname}/products?top=${numberOfProduct}&minPrice=${startRange}&maxPrice=${endRange}`,{
             method : "GET",
             headers: {
                 'Authorization': `Bearer ${token}`
             }
         }).then(response => response.json())
         .then(data => {
+          console.log(data)
            const paginated = []
            for(let i = 0;i<data.length;i+=10){
             paginated.push(data.slice(i,i+10))
            }
            res.json(paginated)
         }).catch(error => {
-            next(error)
-    })
+            next(error)})
 })
 
 router.route("/:sort/:parameter").get((req,res)=>{
